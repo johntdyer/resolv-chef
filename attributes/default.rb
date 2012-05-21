ed4c0a64478b056e366e647efeea8e1d50025337
John Dyer <johntdyer@gmail.com>
2012-05-20 22:31:13 -0400

    Set timeout and attempts

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,4 @@
 default[:resolv][:domain] = node.name.split(".")[1..-1].join(".")  # host.sub.domain.com => sub.domain.com
+
+default[:resolv][:timeout]   =  1
+default[:resolv][:attempts]  =  2
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,12 +7,25 @@
 # All rights reserved - Do Not Redistribute
 #
 
+
+# If there are no 'options' block in resolv.conf we want to add it
+script "add options to resolv.conf if missing" do
+  interpreter "bash"
+  user "root"
+  cwd "/etc"
+  code <<-EOF
+    echo "options rotate timeout:#{node[:resolv][:timeout]} attempts:#{node[:resolv][:attempts]}" >> resolv.conf
+  EOF
+  not_if "grep options /etc/resolv.conf"
+end
+
 ruby_block "edit resolv conf" do
   if node[:resolv][:domain]
     block do
       rc = Chef::Util::FileEdit.new("/etc/resolv.conf")
       rc.search_file_replace_line(/^search/, "search #{node[:resolv][:domain]} #{node[:resolv][:domain].dup.split(".").last(2).join('.') if node[:resolv][:domain].dup.split(".").count>2}")
       rc.search_file_replace_line(/^domain/, "domain #{node[:resolv][:domain]}")
+      rc.search_file_replace_line(/^options/, "options rotate timeout:#{node[:resolv][:timeout]} attempts:#{node[:resolv][:attempts]}")
       rc.write_file
     end
   end

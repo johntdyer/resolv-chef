d9a5bc64e2c83e9b09f1c1512862b2badea9754e
japearson <japearson@agiledigital.com.au>
2012-08-17 12:05:26 +1000

    Add flag to prevent options from being touched

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,14 +9,16 @@
 
 
 # If there are no 'options' block in resolv.conf we want to add it
-script "add options to resolv.conf if missing" do
-  interpreter "bash"
-  user "root"
-  cwd "/etc"
-  code <<-EOF
-    echo "options rotate timeout:#{node[:resolv][:timeout]} attempts:#{node[:resolv][:attempts]}" >> resolv.conf
-  EOF
-  not_if "grep options /etc/resolv.conf"
+if node[:resolv][:options_enabled]
+   script "add options to resolv.conf if missing" do
+     interpreter "bash"
+     user "root"
+     cwd "/etc"
+     code <<-EOF
+       echo "options rotate timeout:#{node[:resolv][:timeout]} attempts:#{node[:resolv][:attempts]}" >> resolv.conf
+     EOF
+     not_if "grep options /etc/resolv.conf"
+   end
 end
 
 ruby_block "edit resolv conf" do
@@ -25,7 +27,9 @@ ruby_block "edit resolv conf" do
       rc = Chef::Util::FileEdit.new("/etc/resolv.conf")
       rc.search_file_replace_line(/^search/, "search #{node[:resolv][:domain]} #{node[:resolv][:domain].dup.split(".").last(2).join('.') if node[:resolv][:domain].dup.split(".").count>2 && node[:resolv][:search_additional_domain]}")
       rc.search_file_replace_line(/^domain/, "domain #{node[:resolv][:domain]}")
-      rc.search_file_replace_line(/^options/, "options rotate timeout:#{node[:resolv][:timeout]} attempts:#{node[:resolv][:attempts]}")
+      if node[:resolv][:options_enabled]
+         rc.search_file_replace_line(/^options/, "options rotate timeout:#{node[:resolv][:timeout]} attempts:#{node[:resolv][:attempts]}")
+      end
       rc.write_file
     end
   end
